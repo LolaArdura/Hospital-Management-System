9dbaeaf5933e60e68c2ef2927e89ce0e9a0fb41b
Lola Ardura <lolaardura99@gmail.com>
2018-02-09 23:31:37 +0100

    employee

--- src/model/Employee.java
+++ src/model/Employee.java
@@ -1,5 +1,48 @@
 package model;
 
 public abstract class Employee {
+	private Integer id;
+	private String name;
+	private byte[] photo;
+	private String schedule;
+	
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public byte[] getPhoto() {
+		return photo;
+	}
+	public void setPhoto(byte[] photo) {
+		this.photo = photo;
+	}
+	public String getSchedule() {
+		return schedule;
+	}
+	public void setSchedule(String schedule) {
+		this.schedule = schedule;
+	}
+  
+	
+	@Override
+	public boolean equals(Object obj) {
+		if(obj==null) return false;
+		if(this==obj) return true;
+		
+		else return false;
+		
+	}
 
+	
+	
+	
 }
