3365ecb7b678bf9a47f2ea5affe8b7567f95f88a
Ines <i.martin59@usp.ceu.es>
2018-02-20 19:23:20 +0100

    Room pojo modify

--- src/model/Room.java
+++ src/model/Room.java
@@ -11,13 +11,16 @@ public class Room implements Serializable {
 	
     private Integer id;
     private Integer number;
-    private String type;
+    private enum typeRoom {
+    	SUITE, DOUBLE, INDIVIDUAL, BOX, UCI
+    };
+    private typeRoom type; 
     private Integer floor;
     private Integer capacity;
 	private float costPerDay;
 	
 	//Constructor
-	public Room (Integer id, Integer number, String type, Integer floor, Integer capacity, float costPerDay) {
+	public Room (Integer id, Integer number, typeRoom type, Integer floor, Integer capacity, float costPerDay) {
 		
 		this.id = id;
 		this.number = number;
@@ -27,7 +30,24 @@ public class Room implements Serializable {
 		this.costPerDay= costPerDay;
 		
 	}
-	
+    public Room ( Integer number, typeRoom type, Integer floor, Integer capacity, float costPerDay) {
+		
+		this.number = number;
+		this.type = type;
+		this.floor= floor;
+		this.capacity= capacity;
+		this.costPerDay= costPerDay;
+		
+	}
+
+    public Room ()	{
+	  number = null;
+	  type = ;
+	  floor = 0;
+	  capacity = 2;
+	  costPerDay = 5;
+    }
+	 
 	//functions gets and sets
 	
 	public Integer getId() {
@@ -42,12 +62,15 @@ public class Room implements Serializable {
 	public void setNumber(Integer number) {
 		this.number = number;
 	}
-	public String getType() {
+	
+	public typeRoom getType() {
 		return type;
 	}
-	public void setType(String type) {
+
+	public void setType(typeRoom type) {
 		this.type = type;
 	}
+
 	public Integer getFloor() {
 		return floor;
 	}
@@ -73,15 +96,10 @@ public class Room implements Serializable {
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((capacity == null) ? 0 : capacity.hashCode());
-		result = prime * result + Float.floatToIntBits(costPerDay);
-		result = prime * result + ((floor == null) ? 0 : floor.hashCode());
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		result = prime * result + ((number == null) ? 0 : number.hashCode());
-		result = prime * result + ((type == null) ? 0 : type.hashCode());
 		return result;
 	}
-	
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -91,42 +109,23 @@ public class Room implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		Room other = (Room) obj;
-		if (capacity == null) {
-			if (other.capacity != null)
-				return false;
-		} else if (!capacity.equals(other.capacity))
-			return false;
-		if (Float.floatToIntBits(costPerDay) != Float.floatToIntBits(other.costPerDay))
-			return false;
-		if (floor == null) {
-			if (other.floor != null)
-				return false;
-		} else if (!floor.equals(other.floor))
-			return false;
 		if (id == null) {
 			if (other.id != null)
 				return false;
 		} else if (!id.equals(other.id))
 			return false;
-		if (number == null) {
-			if (other.number != null)
-				return false;
-		} else if (!number.equals(other.number))
-			return false;
-		if (type == null) {
-			if (other.type != null)
-				return false;
-		} else if (!type.equals(other.type))
-			return false;
 		return true;
 	}
-	
-	
+
 	@Override
 	public String toString() {
 		return "Room [id=" + id + ", number=" + number + ", type=" + type + ", floor=" + floor + ", capacity="
 				+ capacity + ", costPerDay=" + costPerDay + "]";
 	}
+	
+	
+	
+
 	
 	
 }
