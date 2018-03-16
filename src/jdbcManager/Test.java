b61027baa63af096396a6a4c2ac1d0f8b58c981a
ainhoarey <ainhoa1_rey@hotmail.com>
2018-03-16 17:21:22 +0100

    Test

--- src/jdbcManager/Test.java
+++ src/jdbcManager/Test.java
@@ -2,24 +2,20 @@ package jdbcManager;
 import java.util.List;
 
 import model.Doctor;
-import tables.DatabaseTables;
+import model.Nurse;
 
 public class Test {
  public static void main (String[] args) throws Exception {
-	  //create tables
+	 //create tables
 	 //DatabaseTables.createTables(); 
 	
-	 //create a doctor
-	 Doctor doctor_ = new Doctor(06, "Pepe", null , "morning", "cardiology" );
+	 Doctor doctor_ = new Doctor(12, "Pepe", null , "morning", "cardiology" );
 	 DoctorController.getDoctorController().insertDoctor (doctor_);
 	 List<Doctor> a = DoctorController.getDoctorController().getAllDoctors();
 	 System.out.println(a);
 	 List<Doctor> b = DoctorController.getDoctorController().getAllDoctors();
 	 System.out.println(b);
-	Doctor c=DoctorController.getDoctorController().searchDoctorById(6);
-	System.out.println(c);
-	DoctorController.getDoctorController().deleteDoctor(doctor_);
-	 
-	 
+	 Doctor c=DoctorController.getDoctorController().searchDoctorById(11);
+	 System.out.println(c);
  }
 }
