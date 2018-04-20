73e219a01a4e4c49346e37870c82be67b798cc17
alejandragar <35834147+alejandragar@users.noreply.github.com>
2018-04-20 16:21:40 +0200

    nurse JPA

--- src/jpaManager/JPANurseController.java
+++ src/jpaManager/JPANurseController.java
@@ -1,8 +1,10 @@
 package jpaManager;
 
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
 
 import model.Nurse;
+import model.Treatment;
 
 public class JPANurseController {
 	
@@ -31,4 +33,77 @@ private static JPANurseController singleton;
 		em.getTransaction().commit();
 		return true;
 	}
+	
+	public Nurse searchNurseById (Integer id) throws Exception{
+		//get the entity manager
+		EntityManager em= DBEntityManager.getEntityManager();
+		em.getTransaction().begin();
+		em.createNativeQuery("PRAGMA foreign_keys=ON").executeUpdate();
+		em.getTransaction().commit();
+		
+		//search the nurse by id
+		Query q1 = em.createNativeQuery("SELECT * FROM nurse WHERE id LIKE ?", Nurse.class);
+		q1.setParameter(1, id );
+		Nurse nurse = (Nurse)q1.getSingleResult();
+		return nurse;
+	}
+	
+	public Nurse searchNurseBySchedule  (String schedule) throws Exception{
+		//get the entity manager
+		EntityManager em= DBEntityManager.getEntityManager();
+		em.getTransaction().begin();
+		em.createNativeQuery("PRAGMA foreign_keys=ON").executeUpdate();
+		em.getTransaction().commit();
+		
+		//search the nurse by schedule
+		Query q1= em.createNativeQuery("SELECT * FROM nurse WHERE schedule LIKE ?", Nurse.class);
+		q1.setParameter(1, schedule);
+		Nurse nurse = (Nurse)q1.getSingleResult();
+		return nurse;
+	}
+	
+	public Nurse searchNurseByName (String name) throws Exception{
+		//get the entity manager
+		EntityManager em= DBEntityManager.getEntityManager();
+		em.getTransaction().begin();
+		em.createNativeQuery("PRAGMA foreign_keys=ON").executeUpdate();
+		em.getTransaction().commit();
+				
+		//search the nurse by schedule
+		Query q1= em.createNativeQuery("SELECT * FROM nurse WHERE name LIKE ?", Nurse.class);
+		q1.setParameter(1, name);
+		Nurse nurse = (Nurse)q1.getSingleResult();
+		return nurse;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
