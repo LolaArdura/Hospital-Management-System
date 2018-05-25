3f20e005faf0f3f7b79627f2949ac460854a5eee
ainhoarey <ainhoa1_rey@hotmail.com>
2018-05-25 15:41:07 +0200

    xml test

--- src/xmlManager/xmlTest.java
+++ src/xmlManager/xmlTest.java
@@ -24,20 +24,20 @@ public class xmlTest {
 		Room r = new Room (8, 208, roomType.BOX, 2 , 10, 15);
 		LinkedList<Treatment> treatments= new LinkedList<>();
 		Doctor doctor= new Doctor(2,"Harry","morning","inmunollogy");
-		treatments.add(new Treatment(23,"intravenous",dateAdmission,dateAdmission,200,"chemotherapy","deadly dose",doctor));
+		//treatments.add(new Treatment(23,"intravenous",dateAdmission,dateAdmission,200,"chemotherapy","deadly dose",doctor));
 		LinkedList<Nurse> nurses= new LinkedList<>();
-		nurses.add(new Nurse(1,"Lola",null,"morning","injections"));
+		//nurses.add(new Nurse(1,"Lola",null,"morning","injections"));
 		LinkedList<Bills> bills=new LinkedList<>();
-		bills.add(new Bills(1,200, "123456ESB",false));
+		bills.add(new Bills(8,200, "123456ESB",false));
 		Patient p1 = new Patient(2,"Ale", Sex.male,"dead", dob, dateAdmission,treatments,nurses,bills,r);
 		//JPAPatientController.getPatientController().insertNoDiagnosePatient(p1);
 		//List<Patient> pt = JPAPatientController.getPatientController().getAllPatients();
-		//xmlPatient.marshal(p1, "./xmls/Sample-Patients.xml");
+		xmlPatient.marshal(p1, "./xmls/Sample-Patients.xml");*/
 		Patient p=xmlPatient.unmarshal("./xmls/Sample-Patients.xml");
         System.out.println(p);
-        System.out.println(p.getListOfTreatments());
-	*/
-		xmlPatient.xml2Html("./xmls/External-Patient.xml", "./xmls/Patient-Style.xslt", "./xmls/External-Patient.html");
+        //System.out.println(p.getListOfTreatments());
+	
+		//xmlPatient.xml2Html("./xmls/External-Patient.xml", "./xmls/Patient-Style.xslt", "./xmls/External-Patient.html");
 
 	}
 }
