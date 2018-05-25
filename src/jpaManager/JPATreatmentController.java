80d779abaa2285a266974902c777d5429353b3f7
ainhoarey <ainhoa1_rey@hotmail.com>
2018-05-25 15:26:12 +0200

    try and catch in treatment

--- src/jpaManager/JPATreatmentController.java
+++ src/jpaManager/JPATreatmentController.java
@@ -18,24 +18,34 @@ public class JPATreatmentController implements TreatmentInterface {
 	 }
 	 public boolean insertTreatment (Treatment treatment) throws Exception{
 		 EntityManager em =DBEntityManager.getEntityManager();
+		 try {
 		 em.getTransaction().begin();
 		 em.persist(treatment);
 		 em.getTransaction().commit();
 		 return true;
-		 
+		 }catch(Exception e) {
+			 em.getTransaction().commit();
+			 throw new Exception();
+		 }
 	 }
 	 
 	 //NOT SURE
 	 public void insertTreatmentWithoutBill (Treatment treatment) throws Exception{
 		 EntityManager em =DBEntityManager.getEntityManager();
+		 try {
 		 em.getTransaction().begin();
 		 em.persist(treatment);
 		 em.getTransaction().commit();
+		 }catch(Exception e) {
+			 em.getTransaction().commit();
+			 throw new Exception();
+		 }
 	 }
 	 
 	 public boolean deleteTreatment (Treatment treatment) throws Exception{
 		 
 		 EntityManager em =DBEntityManager.getEntityManager();
+		 try {
 		 Treatment treatmentRetrieved= searchTreatmentById(treatment.getId());
 		 em.getTransaction().begin();
 		 treatmentRetrieved.getPatient().removeTreatment(treatmentRetrieved);
@@ -45,31 +55,39 @@ public class JPATreatmentController implements TreatmentInterface {
 		 em.remove(treatmentRetrieved);
 		 em.getTransaction().commit();
 		 return true;
-		 
+		 }catch(Exception e) {
+			 em.getTransaction().commit();
+			 throw new Exception();
+		 }
 	 }
 	 
 	 
 	public Treatment searchTreatmentById (Integer id) throws Exception{
-		// Get the entity manager
 		EntityManager em= DBEntityManager.getEntityManager();
+		try {
 		em.getTransaction().begin();
 		em.createNativeQuery("PRAGMA foreign_keys=ON").executeUpdate();
 		em.getTransaction().commit();
-		
-		//Search the treatments by id
 		Query q1 = em.createNativeQuery("SELECT * FROM treatment WHERE id LIKE ?", Treatment.class);
 		q1.setParameter(1, id );
 		Treatment treatment = (Treatment)q1.getSingleResult();
 		return treatment;
-		
-		}
+		}catch(Exception e) {
+			 em.getTransaction().commit();
+			 throw new Exception();
+		 }
+	}
 	
 	
 	public void updateTreatment(Treatment treatment) throws Exception {
 		EntityManager em=DBEntityManager.getEntityManager();
+		try {
 		em.getTransaction().begin();
 		em.flush();
 		em.getTransaction().commit();
+		}catch(Exception e) {
+			 em.getTransaction().commit();
+			 throw new Exception();
+		 }
 	}
-		
-	}
+}
