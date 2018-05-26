524794e527c7c4cd410a321aa7e7d6b8517b2aaf
Ines <i.martin59@usp.ceu.es>
2018-05-26 19:54:58 +0200

    roomController update tested

--- src/jpaManager/TestJPA.java
+++ src/jpaManager/TestJPA.java
@@ -45,20 +45,36 @@ public class TestJPA {
 		
 		
 		 //TEST ROOMCONTROLLE METHODS
-		/* 
-		 //insert Room without list of patient
-		 Room room1 = new Room (1, 200, "suite", 2, 3, 15 );
-		 Room room2 = new Room (2, 102, "normal", 2, 3, 5);
-		 Room room3 = new Room (3, 201, "normal", 2, 3, 5 ); 
-		//JPARoomController.getJPARoomController().insertRoom(room1);
-		//JPARoomController.getJPARoomController().insertRoom(room2);
-		// JPARoomController.getJPARoomController().insertRoom(room3);
 		 
+		// insert Room without list of patient 
+	
+	 //Comprobacion Update room
+	     /* Room room1 = new Room (5, 200, "suite", 2, 3, 15 );
+		 Room room2 = new Room (33, 102, "normal", 2, 3, 5);
+		 Room room3 = new Room (20, 201, "normal", 2, 3, 5 ); 
+         Room room4 = new Room (1, 22, "box",1 ,2 ,8);
+        // JPARoomController.getJPARoomController().insertRoom(room4);
+		 room4.setNumber(55);
+		 JPARoomController.getJPARoomController().updateRoom(room4);
+		 List<Room> rList= JPARoomController.getJPARoomController().getAllRooms();
+			for (Room r : rList) {
+				System.out.println(r);
+			}*/
+		///JPARoomController.getJPARoomController().insertRoom(room1);
+		//JPARoomController.getJPARoomController().insertRoom(room2);
+		 //JPARoomController.getJPARoomController().insertRoom(room3);
+		/* List<Room> rList= JPARoomController.getJPARoomController().getFreeRooms();
+		for (Room r : rList) {
+			System.out.println(r);
+		}*/
+		 
+		
+		
 		 //delete room
 		// JPARoomController.getJPARoomController().deleteRoom(room2);
 		 
 		 //getAllRooms
-		 List <Room> roomList = JPARoomController.getJPARoomController().getAllRooms();
+		 /*List <Room> roomList = JPARoomController.getJPARoomController().getAllRooms();
 		 for (Room room: roomList ) {
 			 System.out.println(room);
 	 	 }/*
@@ -87,7 +103,7 @@ public class TestJPA {
 	 
 	 
 	 //DatabaseTables.createTables();
-	 Date dob = Date.valueOf(LocalDate.of(1999,1, 7));
+	/* Date dob = Date.valueOf(LocalDate.of(1999,1, 7));
 	 Date dateAdmission = new Date (24/04/2018) ;
 /*	 Patient patient1 = new Patient ( "Ale", Sex.female,"pneumonia",dob,  dateAdmission );
 
@@ -125,7 +141,7 @@ public class TestJPA {
 	// Patient Lolita = new Patient (1,"Lolita", Sex.female, "mental disorder", dob, dateAdmission);
 	// JPAPatientController.getPatientController().insertCompletePatient(Lolita);
 	// System.out.println(Lolita);
-	 Patient Lolita = new Patient (1,"Lolita", Sex.female, "mental disorder", dob, dateAdmission);
+	// Patient Lolita = new Patient (1,"Lolita", Sex.female, "mental disorder", dob, dateAdmission);
 	 //JPAPatientController.getPatientController().insertCompletePatient(Lolita);
 	 //System.out.println(Lolita);
 /*	
@@ -201,7 +217,7 @@ public class TestJPA {
 */	
 	 
 	//TEST JPANURSECONTROLLER METHODS:
-	Nurse nurse1 = new Nurse(1, "Paquita", "morning", "injections" );
+	//Nurse nurse1 = new Nurse(1, "Paquita", "morning", "injections" );
 /*	Nurse nurse2 = new Nurse(2, "Florencia", "evening", "injections" );
 	Nurse nurse3 = new Nurse(3, "Maxima", "night", "injections" );
 	//insertNurse:
@@ -229,9 +245,9 @@ public class TestJPA {
 	 //addPatientToNurse
 	// JPANurseController.getNurseController().addPatientToNurse(nurse1, Lolita);
 	// System.out.println(JPANurseController.getNurseController().getAllNurses());
-	 JPANurseController.getNurseController().addPatientToNurse(nurse1, Lolita);
+	/* JPANurseController.getNurseController().addPatientToNurse(nurse1, Lolita);
 	 
-	 System.out.println(JPANurseController.getNurseController().getAllNurses());
+	 System.out.println(JPANurseController.getNurseController().getAllNurses());*/
 	 
 	 
 	 
