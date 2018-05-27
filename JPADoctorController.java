e8cb1077bd178de4d7b6a0db990403593e19519a
Lola Ardura <lolaardura99@gmail.com>
2018-05-27 10:33:47 +0200

    .

---
+++ JPADoctorController.java
@@ -0,0 +1,147 @@
+package jpaManager;
+
+import java.util.*;
+
+import javax.persistence.*;
+
+import interfaces.DoctorInterface;
+import model.Doctor;
+
+public class JPADoctorController implements DoctorInterface{
+
+	private static JPADoctorController singleton;
+	
+	public static JPADoctorController getJPADoctorController() {
+		if(singleton == null) {
+			singleton = new JPADoctorController();
+		}
+		return singleton;
+	}
+	
+	public boolean insertDoctor(Doctor doctor) throws Exception{
+		EntityManager em = DBEntityManager.getEntityManager();
+		try {
+		em.getTransaction().begin();
+		em.persist(doctor);
+		em.getTransaction().commit();
+		return true;
+		}catch(Exception e) {
+			em.getTransaction().commit();
+			throw new Exception();
+		}
+	}
+	
+	public boolean deleteDoctor(Doctor doctor) throws Exception{
+		EntityManager em = DBEntityManager.getEntityManager();
+		try {
+			Doctor doctorReceived=JPADoctorController.getJPADoctorController().searchDoctorById(doctor.getId());
+			em.getTransaction().begin();
+			em.remove(doctorReceived);
+			em.getTransaction().commit();
+		return true;
+		}catch(Exception e) {
+			e.printStackTrace();
+			em.getTransaction().commit();
+			throw new Exception();
+		 }
+	}
+	
+	public Doctor searchDoctorById (Integer id) throws Exception{
+		EntityManager em = DBEntityManager.getEntityManager();
+		try {
+		em.getTransaction().begin();
+		em.createNativeQuery("PRAGMA foreign_keys=ON").executeUpdate();
+		em.getTransaction().commit();
+		Query q1 = em.createNativeQuery("SELECT * FROM doctor WHERE id = ?", Doctor.class);
+		q1.setParameter(1, id);
+		Doctor doctor = (Doctor) q1.getSingleResult();
+		return doctor;
+		}catch(Exception e) {
+			 em.getTransaction().commit();
+			 throw new Exception();
+		 }
+	}
+	
+	public List<Doctor> getAllDoctors() throws Exception{
+		EntityManager em = DBEntityManager.getEntityManager();
+		try {
+		em.getTransaction().begin();
+		em.createNativeQuery("PRAGMA foreign_keys=ON").executeUpdate();
+		em.getTransaction().commit();
+		Query q1 = em.createNativeQuery("SELECT * FROM doctor", Doctor.class);
+		List <Doctor> doctors = (List<Doctor>) q1.getResultList();
+		return doctors;
+		}catch(Exception e) {
+			 em.getTransaction().commit();
+			 throw new Exception();
+		 }
+	}
+
+
+	@Override
+	public List<Doctor> searchDoctorByName(String name) throws Exception {
+		EntityManager em = DBEntityManager.getEntityManager();
+		try {
+		em.getTransaction().begin();
+		em.createNativeQuery("PRAGMA foreign_keys=ON").executeUpdate();
+		em.getTransaction().commit();
+		Query q1 = em.createNativeQuery("SELECT * FROM doctor WHERE name LIKE ?", Doctor.class);
+		q1.setParameter(1, name);
+		List<Doctor> doctor = (List<Doctor>) q1.getResultList();
+		return doctor;
+		}catch(Exception e) {
+			 em.getTransaction().commit();
+			 throw new Exception();
+		 }
+	}
+
+	@Override
+	public List<Doctor> searchDoctorBySpecialty(String specialty) throws Exception {
+		EntityManager em = DBEntityManager.getEntityManager();
+		try {
+		em.getTransaction().begin();
+		em.createNativeQuery("PRAGMA foreign_keys=ON").executeUpdate();
+		em.getTransaction().commit();
+		Query q1 = em.createNativeQuery("SELECT * FROM doctor WHERE specialty LIKE ?", Doctor.class);
+		q1.setParameter(1, specialty);
+		List<Doctor> doctor = (List<Doctor>) q1.getResultList();
+		return doctor;
+		}catch(Exception e) {
+			 em.getTransaction().commit();
+			 throw new Exception();
+		 }
+	}
+
+	@Override
+	public List<Doctor> searchDoctorBySchedule(String schedule) throws Exception {
+		EntityManager em = DBEntityManager.getEntityManager();
+		try {
+		em.getTransaction().begin();
+		em.createNativeQuery("PRAGMA foreign_key=ON").executeUpdate();
+		em.getTransaction().commit();
+		Query q1 = em.createNativeQuery("SELECT * FROM doctor WHERE schedule LIKE ?", Doctor.class);
+		q1.setParameter(1, schedule);
+		List<Doctor> doctor = (List<Doctor>) q1.getResultList();
+		return doctor;
+		}catch(Exception e) {
+			 em.getTransaction().commit();
+			 throw new Exception();
+		 }
+	}
+
+	@Override
+	public void updateDoctor(Doctor doctor) throws Exception {
+		EntityManager em=DBEntityManager.getEntityManager();
+		try {
+		em.getTransaction().begin();
+		em.flush();
+		em.getTransaction().commit();
+		}catch(Exception e) {
+			 em.getTransaction().commit();
+			 throw new Exception();
+		 }
+	}
+	
+	
+	
+}
